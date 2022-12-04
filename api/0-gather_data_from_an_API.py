920d514095f5c08c71a15615e115cbc4ff215801
root <you@example.com>
2022-12-04 05:47:06 -0800

    pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """given employee ID, returns information about his/her TODO list progress."""
 
-
-import requests
 import json
+import requests
 import sys
 
 if __name__ == "__main__":
-    #ALL MY VARIABLE
+    """ALL MY VARIABLE"""
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     users = requests.get('https://jsonplaceholder.typicode.com/users')
     todostxt = todos.text
@@ -16,28 +15,27 @@ if __name__ == "__main__":
     usersjson = json.loads(userstxt)
     Id = sys.argv[1]
     final = []
-    finaltask =[]
+    finaltask = []
     NUMBER_OF_DONE_TASKS = 0
     taskleft = 0
 
-    #FOR RETRIEVE TASK_ID
+    """FOR RETRIEVE TASK_ID"""
     for key in usersjson:
         if key['id'] == int(Id):
             EMPLOYEE_NAME = key['name']
 
-    #FOR RETRIEVE USER_ID
+    """FOR RETRIEVE USER_ID"""
     for value in todosjson:
         if value['userId'] == int(Id):
             final.append(value)
 
     for taskdone in final:
-        if taskdone['completed'] == True:
+        if taskdone['completed'] is True:
             finaltask.append(taskdone['title'])
             NUMBER_OF_DONE_TASKS = NUMBER_OF_DONE_TASKS + 1
 
-
-    print("Employee {} is done with tasks({}/20)".format(EMPLOYEE_NAME, 
-                                                        NUMBER_OF_DONE_TASKS))
+    print("Employee {} is done with tasks({}/20)".format(EMPLOYEE_NAME,
+                                                         NUMBER_OF_DONE_TASKS))
 
     for taskfinal in finaltask:
         print("\t {}".format(taskfinal))
